f4abcfac3b2208452a5e0949f223e6b9f4c06373
haimbacharr <81071510+haimbacharr@users.noreply.github.com>
2022-07-20 23:35:09 +0300

    add file to .gitignore fix bug when press back from main activtiy change openScreen activity class

--- app/src/main/java/com/example/finalproject/OpenScreen.java
+++ app/src/main/java/com/example/finalproject/OpenScreen.java
@@ -3,6 +3,7 @@ package com.example.finalproject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,18 +12,20 @@ import java.util.TimerTask;
 
 public class OpenScreen extends Activity {
 
-    //Open the 'OpenScreen' application for 4 sec and go to mainActivity by using new Intent
-    //we add the "OpenScreen" in the manifest.xml as MIAN
-    //and add the "MainActivity" as   <category android:name="android.intent.category.DEFAULT" />
+    private static int SPLASH_TIME_OUT = 3000;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.open_screen_app);
-        new Timer().schedule(new TimerTask() {
+
+        new Handler().postDelayed(new Runnable() {
+
             @Override
             public void run() {
-                startActivity(new Intent(OpenScreen.this ,MainActivity.class ));
+                startActivity(new Intent(OpenScreen.this, MainActivity.class));
+                OpenScreen.this.finish();
             }
-        }, 4000);
+        }, SPLASH_TIME_OUT);
     }
 }
